d33031772b06370d17012465d34a6d71821d82ae
josemar <josemar7@gmail.com>
2018-05-02 13:36:24 +0200

    mi commit!

---
+++ src/components/Persons/Persons.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
+import Person from './Person/Person';
+
+class Persons extends Component {
+
+    constructor(props) {
+        super(props);
+        console.log('[Persons.js] Inside Contructor', props);
+      }
+    
+      componentWillMount() {
+        console.log('[Persons.js] Inside componentWillMount()');
+      }
+    
+      componentDidMount() {
+        console.log('[Persons.js] Inside componentDidMount()');
+      }
+    
+    render() {
+        console.log('[Persons.js] Inside render()');
+        return this.props.persons.map((person, index) => {
+            return <ErrorBoundary key={person.id}><Person 
+                    click={() => this.props.clicked(index)}
+                    name={person.name} 
+                    age={person.age}                    
+                    changed={(event) => this.props.changed(event, person.id)} />
+                    </ErrorBoundary>
+          });    
+    }
+}
+
+export default Persons;      
